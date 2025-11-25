a5218943289b7942e361f50c92ac2373f3666e43
erwinautier <erwin.autier@gmail.com>
2025-11-25 14:56:49 +0100

    Update appV3.py

--- appV3.py
+++ appV3.py
@@ -337,7 +337,7 @@ def render_range_grid(spot_def, highlight_hand=None):
             # Surlignage de la main fautive (tout le fond de la cellule)
             highlight_style = ""
             if highlight_hand is not None and hand == highlight_hand:
-                highlight_style = "background-color:#E5E7EB; border-radius:4px;"
+                highlight_style = "background-color:#EB3024; border-radius:4px;"
 
             cell_html = f"""
             <td style="padding:1px; text-align:center; {highlight_style}">
