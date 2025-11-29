f3a61c47b8928b6d0a89c560c83469e1c0c8b379
erwinautier <erwin.autier@gmail.com>
2025-11-29 16:54:52 +0100

    Update PPC-APPV3.py

--- PPC-APPV3.py
+++ PPC-APPV3.py
@@ -5,6 +5,17 @@ import hashlib
 
 import streamlit as st
 
+from supabase import create_client, Client
+
+@st.cache_resource
+def get_supabase() -> Client:
+    url = st.secrets["supabase"]["url"]
+    key = st.secrets["supabase"]["anon_key"]
+    return create_client(url, key)
+
+supabase = get_supabase()
+
+
 # =========================================================
 # Configuration globale
 # =========================================================
